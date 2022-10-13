ecd4bab8ae883632e03afb4685c4ba8ce5f51970
Andrey-Legion <andrey.legion@gmail.com>
2022-10-13 19:31:24 +0300

    ДЗ Циклы Часть1

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         }
         System.out.println("\n");
         //Task 1-3
-        for (int i=0; i<=17; i=i+2) {
+        for (int i=0; i<=17; i+=2) {
             System.out.print (i + " ");
         }
         System.out.println("\n");
@@ -25,19 +25,19 @@ public class Main {
         System.out.println("\n");
 
         //Task 2-1
-        for (int y=1904; y<=2096; y=y+4){
+        for (int y=1904; y<=2096; y+=4) {
             System.out.println( y + " год является високосным");
         }
         System.out.println("\n");
 
         //Task 2-2
-        for (int num= 7; num<=98; num=num+7) {
+        for (int num= 7; num<=98; num+=7) {
             System.out.print(num + " ");
         }
         System.out.println("\n");
 
         //Task 2-3
-        for (int num=1; num<=512; num=num*2) {
+        for (int num=1; num<=512; num*=2) {
             System.out.print(num + " ");
         }
         System.out.println("\n");
